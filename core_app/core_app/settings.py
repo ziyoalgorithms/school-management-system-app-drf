5185a3cca303d494a965ba0d359388ea218fd3bb
Ziyodullokhon <studiovisual0077@gmail.com>
2022-09-23 16:36:03 -0400

    Deploy 10

--- core_app/core_app/settings.py
+++ core_app/core_app/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,6 +92,11 @@ DATABASES = {
         'PORT': os.environ.get('PORT'),
     }
 }
+
+
+
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 
 # Password validation
